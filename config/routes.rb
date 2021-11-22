d6c39565a57f5c5d11397af8fa648820753ade62
JUNoym <junnyaooyama@gmail.com>
2021-11-23 06:42:10 +0900

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # delete '/todos/destroy_all', to: 'todos#destroy_all'
+      delete '/todos/destroy_all', to: 'todos#destroy_all'
       resources :todos
     end
   end
