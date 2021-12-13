a562117652f7e5b66c94c89cd4db1d700fc90799
JUNoym <junnyaooyama@gmail.com>
2021-12-14 01:15:57 +0900

    時間表示エラー修正

--- app/controllers/api/v1/user_care_actions_controller.rb
+++ app/controllers/api/v1/user_care_actions_controller.rb
@@ -2,7 +2,17 @@ class Api::V1::UserCareActionsController < ApplicationController
   def index
     users = User.all
     res = users.map{ |user|
-      {"user_name"=>user.name,"user_id"=>user.id,"care_actions"=>user.care_action}
+      # user_idに紐づく中間テーブル（UserCareAction）の一覧を取得
+      user_care_actions = UserCareAction.where(user_id:user.id)
+      result = user_care_actions.map{ |action|
+        # UserCareActionそれぞれについて、care_action_idから、CareActionの情報を取得
+        care_action = CareAction.find(action.care_action_id)
+        # ↑で取得したCareActionのtimestampはシードが作られた時間になるので、中間テーブルの時間で上書き
+        care_action.created_at = action.created_at
+        care_action.updated_at = action.updated_at
+        next care_action
+      }
+      next {"user_name"=>user.name,"user_id"=>user.id,"care_actions"=>result}
     }
     render json: res
   end
@@ -23,8 +33,7 @@ class Api::V1::UserCareActionsController < ApplicationController
     user = User.find(params[:user_id])
     user_care_action = user.user_care_action.new(user_id:user.id,care_action_id:care_action.id)
     if user_care_action.save
-      care_actions = user.care_action
-      render json: care_actions
+      render json: user_care_action
     end
   end
 
