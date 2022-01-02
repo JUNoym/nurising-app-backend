e5ae11d6b846471789f27a5a1bb2ce14081a01cd
JUNoym <junnyaooyama@gmail.com>
2022-01-02 19:22:52 +0900

    fix

--- app/controllers/api/v1/vital_users_controller.rb
+++ app/controllers/api/v1/vital_users_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::VitalUsersController < ApplicationController
     def index
         # vital_user = VitalUser.where(created_at: Time.now.all_day)
         # render json: vital_user
-        render json: VitalUser.all
+        vital_user = VitalUser.all
+        render json: vital_user
     end
 
     def create
