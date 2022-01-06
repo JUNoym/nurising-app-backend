3e6b4c8e603a26b8f3400166e8f32865bbfb2ad3
JUNoym <junnyaooyama@gmail.com>
2022-01-07 04:03:33 +0900

    session_store.rb削除

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      origins 'http://localhost:8080'
+
+      resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
   end
 end
