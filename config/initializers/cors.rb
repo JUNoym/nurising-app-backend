c038201d3d19d64279497f521bf67a729699b1d7
JUNoym <junnyaooyama@gmail.com>
2022-01-07 03:09:44 +0900

    cors-fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,18 +11,17 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
   # 本番環境
-  allow do
+  # allow do
     # origins 'https://nursing-app-frontend3.vercel.app/'
-     origins '*'
+    #  origins '*'
 
-    resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-  end
+    # resource '*',
+        # headers: :any,
+        # methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        # credentials: true
+  # end
 end
