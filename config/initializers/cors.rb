68f0d5ba8390307a677327344ccce7b1f4d468c0
JUNoym <junnyaooyama@gmail.com>
2022-01-07 04:33:44 +0900

    corsFix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-      origins '*'
+      origins 'http://localhost:8080'
+
       resource '*',
           headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
   end
+
+  allow do
+    origins 'https://nursing-app-frontend3.vercel.app'
+
+    resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+end
 end
