021e2949322ff1e30b65b824037616f6db0b9cea
JUNoym <junnyaooyama@gmail.com>
2022-02-10 00:53:36 +0900

    corsエラー修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://nursing-app-frontend3.vercel.app'
+    origins '*'
 
     resource '*',
         headers: :any,
