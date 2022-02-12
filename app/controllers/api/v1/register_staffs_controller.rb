93d718348c2e022bff6982cf5a40d677297f6cb7
JUNoym <junnyaooyama@gmail.com>
2022-02-13 01:57:06 +0900

    register_staffs API疎通

---
+++ app/controllers/api/v1/register_staffs_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::RegisterStaffsController < ApplicationController
+    def index
+        user = RegisterStaff.all
+        render json: user
+    end
+
+    def create
+        user = RegisterStaff.new(user_param)
+        user.save
+        render json: user
+    end
+
+    private
+        def user_param
+            params.permit(
+                :name, :have_work, :work_in, :work_out
+            )
+        end
+end
+  
