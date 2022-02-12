b180084468db4a3175f4c9f255423fa544396bd4
JUNoym <junnyaooyama@gmail.com>
2022-02-12 19:15:05 +0900

    スタッフ登録テスト成功

--- spec/models/register_staff_spec.rb
+++ spec/models/register_staff_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RegisterStaff, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "出勤するスタッフを登録することができる" do
+    user = RegisterStaff.new(name: "武田", have_work: true, work_in: "2022-02-12 19:07:28.204208", work_out: "2022-02-12 21:07:28.204208")
+    expect(user.format_staff_data).to eq "武田さんの出勤状態は trueです。 "
+  end
 end
