c845bdea719f3cfd7429aa46f53654778ca3dc3b
JUNoym <junnyaooyama@gmail.com>
2022-03-12 03:39:26 +0900

    コメント追加

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     end
 
     def create
+        # http://localhost:3030/api/v1/sessions?email=saisyuu@gmail.com&password=02030203ii
         user = AuthUser.find_by(email:login_params[:email])
         if user && user.authenticate(login_params[:password])
             render json: {status: 'SUCCESS', message:'ログインに成功しました', data:user}
