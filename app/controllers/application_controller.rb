e6ad9b6911c1b762afb9ece601e98cb7d672d53a
JUNoym <junnyaooyama@gmail.com>
2022-03-14 04:49:42 +0900

    devise_parameter_sanitizerをbefore_actionで呼び出すことでnameに追加することができた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
+        before_action :configure_permitted_parameters, if: :devise_controller?
         include DeviseTokenAuth::Concerns::SetUserByToken
+        private
+                def configure_permitted_parameters
+                        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+                end
 end
