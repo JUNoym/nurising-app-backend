0f78c2b2ca218eb0eab7f394970e65f3357c91a1
JUNoym <junnyaooyama@gmail.com>
2022-03-14 06:55:42 +0900

    ログインしているユーザーとすべてのユーザーを返すように実装

--- app/controllers/api/v1/get_users_controller.rb
+++ app/controllers/api/v1/get_users_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::GetUsersController < ApplicationController
-    before_action :authenticate_devise_user!
+    # before_action :authenticate_devise_user!
 
   def index
     render json: {
       data: {
-        user: current_devise_user 
+        user: current_devise_user,
+        res: DeviseUser.all
       }
     }, status: 200
   end
